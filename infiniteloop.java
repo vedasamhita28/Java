28666e6ce91cfea6bebe96f4fd0a325993287c0e
Vedasamhita P <vaishnaviveda28@gmail.com>
2025-02-03 21:35:37 +0530

    Add files via upload

---
+++ infiniteloop.java
@@ -0,0 +1,18 @@
+public class InfiniteLoops {
+    public static void main(String[] args) {
+        // Infinite for loop
+        System.out.println("Infinite for loop:");
+        for (;;) { // No condition to stop the loop
+            System.out.println("This is an infinite for loop!");
+        }
+
+        // Infinite while loop
+        // Uncomment to test the infinite while loop
+        /*
+        System.out.println("Infinite while loop:");
+        while (true) { // Condition always true
+            System.out.println("This is an infinite while loop!");
+        }
+        */
+    }
+}
