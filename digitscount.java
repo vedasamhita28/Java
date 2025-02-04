bb9233f40b24708ac33c108c2b17737f49035c79
Vedasamhita P <vaishnaviveda28@gmail.com>
2025-02-04 20:13:51 +0530

    digitscount.java

--- digitscount.java
+++ digitscount.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DigitCounter {
+public class digitscount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
