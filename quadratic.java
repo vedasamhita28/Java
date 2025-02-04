f446943e456bea0fcfe91c8b67bd85f179c0baef
Vedasamhita P <vaishnaviveda28@gmail.com>
2025-02-04 20:18:16 +0530

    quadratic.java

--- quadratic.java
+++ quadratic.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class QuadraticEquationSolver {
+public class quadratic  {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
