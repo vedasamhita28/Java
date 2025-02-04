96998a93d248e5e99c00dd9af21f4458637a4544
Vedasamhita P <vaishnaviveda28@gmail.com>
2025-02-04 20:13:07 +0530

    calci.java

--- calci.java
+++ calci.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class SimpleCalculator {
+public class calci
+    {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
