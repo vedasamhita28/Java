3d8690dc54effe96ae89c73d8aea71c48f44835a
Vedasamhita P <vaishnaviveda28@gmail.com>
2025-02-04 20:25:02 +0530

    switchcalci.java

--- switchcalci.java
+++ switchcalci.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VersatileCalculator {
+public class switchcalci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
