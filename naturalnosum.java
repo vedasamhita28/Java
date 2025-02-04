86721522c3fb672338051e81f4260dc76d0d6fc3
Vedasamhita P <vaishnaviveda28@gmail.com>
2025-02-04 20:17:06 +0530

    naturalnosum.java

--- naturalnosum.java
+++ naturalnosum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NaturalNumbersSum {
+public class naturalnosum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
