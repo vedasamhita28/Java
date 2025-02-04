170a543f3e5947b02cfeb17da193e92e9c497471
Vedasamhita P <vaishnaviveda28@gmail.com>
2025-02-04 20:19:07 +0530

    sequence.java

--- sequence.java
+++ sequence.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NumberSequence {
+public class sequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
